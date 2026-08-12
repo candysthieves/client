787020c6e1e43c12dea08ddbb0c3c830d28b6372
Intrstng <intrstg@gmail.com>
2026-08-12 19:26:31 +0300

    SCRUM-60 fix: remove сe clsx from dependecies

--- src/app/(auth)/privacy-policy/page.tsx
+++ src/app/(auth)/privacy-policy/page.tsx
@@ -1,7 +1,4 @@
-'use client'
-
-import { ArrowBackOutline, Typography } from '@candy.thieves/ui-kit-lumos'
-import clsx from 'clsx'
+import { ArrowBackOutline, clsx, Typography } from '@candy.thieves/ui-kit-lumos'
 import Link from 'next/link'
 import s from './page.module.scss'
 

752f4bf177496491b3b0cdd405c3b191fb24eab4
Intrstng <intrstg@gmail.com>
2026-08-17 14:37:27 +0300

    SCRUM-29 refactor: add Suspense to vaerify and create-new-password pages

---
+++ src/app/(auth)/verify/VerifyContent.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { useRouter, useSearchParams } from 'next/navigation'
+import { useCallback, useEffect } from 'react'
+import { ApiError, registrationConfirmation } from '@/lib/api'
+import { mapRegistrationConfirmationError } from '@/lib/utils'
+
+export const VerifyContent = () => {
+  const searchParams = useSearchParams()
+  const router = useRouter()
+  const code = searchParams.get('code')
+
+  const handleVerification = useCallback(async () => {
+    /**
+     * If user opened link /verify by himself without code
+     */
+    if (!code) {
+      router.replace('/verification-expired')
+      return
+    }
+
+    try {
+      await registrationConfirmation({ code })
+      router.replace('/verification-success')
+    } catch (error) {
+      if (error instanceof ApiError) {
+        const redirectTo = mapRegistrationConfirmationError(error)
+        if (redirectTo) {
+          router.replace(redirectTo)
+          return
+        }
+      }
+      // здесь уже можно показать generic error
+      console.error(error)
+      throw error
+    }
+  }, [code, router])
+
+  useEffect(() => {
+    void handleVerification()
+  }, [handleVerification])
+
+  return null
+}

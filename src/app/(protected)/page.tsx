cd2265e1236c259aa2aa5e524ba9b7e347c9010b
hoakiin <millychak89@gmail.com>
2026-08-20 22:52:40 +0300

    SCRUM-88 refactor: clean up legacy page styles

--- src/app/(protected)/page.tsx
+++ src/app/(protected)/page.tsx
@@ -1,11 +1,10 @@
 import { Button } from '@candy.thieves/ui-kit-lumos'
 import Link from 'next/link'
-import s from './page.module.css'
 
 export default function Home() {
   return (
-    <main className={s.container}>
-      <h1 className={s.title}>Welcome to LumosApp</h1>
+    <main>
+      <h1>Welcome to LumosApp</h1>
       <Link href={'/sign-up'} style={{ marginRight: 16 }}>
         <Button>Sign up</Button>
       </Link>

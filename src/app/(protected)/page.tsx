cdcb7acfe4c5bb3b1f7fed0147ec410829d93449
hoakiin <millychak89@gmail.com>
2026-08-26 17:23:58 +0300

    SCRUM-122 chore: remove open post button from main page

--- src/app/(protected)/page.tsx
+++ src/app/(protected)/page.tsx
@@ -1,15 +1,9 @@
 'use client'
 
-import { Button, Typography } from '@candy.thieves/ui-kit-lumos'
+import { Button } from '@candy.thieves/ui-kit-lumos'
 import Link from 'next/link'
-import { useState } from 'react'
-import { PostDetailsModal } from '@/components/PostDetailsModal/PostDetailsModal'
-import { mockPost } from '@/components/PostDetailsModal/PostDetailsModal.mock'
-import { PostModal } from '@/components/PostModal/PostModal'
 
 export default function Home() {
-  const [isOpen, setIsOpen] = useState(false)
-
   return (
     <main>
       <h1>Welcome to LumosApp</h1>
@@ -19,10 +13,6 @@ export default function Home() {
       <Link href={'/sign-in'} style={{ marginRight: 16 }}>
         <Button>Sign in</Button>
       </Link>
-
-      <Button onClick={() => setIsOpen(true)}>Show Post</Button>
-
-      <PostModal post={mockPost} open={isOpen} onClose={() => setIsOpen(false)} />
     </main>
   )
 }

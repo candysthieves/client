81ec2e4fd40aff6871fbca6d5e9f46d1a738a33e
Andrei <107413715+Intrstng@users.noreply.github.com>
2026-08-26 17:53:35 +0300

    feat/SCRUM-152: add server to mock Posts requests (#31)
    * SCRUM-108 feat: add QueryProviders
    
    * SCRUM-108 feat: change ProtectedShell add PublicShell for routes
    
    * SCRUM-108 feat: add description to request.ts with 498 invalid refresh token error
    
    * SCRUM-108 chore: check PublicShell and ProtectedShell common work
    
    * SCRUM-108 fix: change global error handling in request.ts
    
    * SCRUM-108 refactor: change client fetch requests to Tanstack query
    
    * SCRUM-152 feat: add temporary server to mock posts requests
    
    * SCRUM-152 refactor: delete unused db.json file
    
    * SCRUM-152 feat: add width / height properties to post mocked data in db.json file
    
    * SCRUM-152 fix: change preview image sizes in mocked post data
    
    * SCRUM-152 fix: change preview image in post mocked data

--- src/app/(protected)/page.tsx
+++ src/app/(protected)/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 
-import { Button, Typography } from '@candy.thieves/ui-kit-lumos'
-import Link from 'next/link'
+import { Typography } from '@candy.thieves/ui-kit-lumos'
 
 export default function Home() {
   return (

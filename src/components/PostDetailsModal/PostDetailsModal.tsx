7aad9580b3509c1f2199fede8dbef58db307135d
hoakiin <134519279+hoakiin@users.noreply.github.com>
2026-08-26 19:47:21 +0300

    feat/SCRUM-122: create post view modal (#28)
    * SCRUM-122 feat: implement post details modal UI with mock data
    
    * SCRUM-122 fix: hide answer button on post description
    
    * SCRUM-122 feat: add guest layout for post details modal
    
    * SCRUM-122 feat: add edit post modal with view/edit mode switching
    
    * SCRUM-122 chore: update ui kit
    
    * SCRUM-122 feat: add close confirmation for edit post modal
    
    * SCRUM-122 fix: make post modals responsive on tablet
    
    * SCRUM-122 fix: render select dropdon above fixed header
    
    * SCRUM-122 fix: remove dublicating styles
    
    * SCRUM-122 feat: adapt post image area to first image aspect ratio
    
    * SCRUM-122 refactor: extract shared post actions and comments
    
    * SCRUM-122 feat: add ActionMenu
    
    * SCRUM-122 fix: adjust padding and remove responsive styles
    
    * SCRUM-122 fix: adjust modal border-radius
    
    * SCRUM-122 fix: adjust modal width
    
    * SCRUM-122 fix: lockfile
    
    * SCRUM-122 fix: show ActionMenu only for post author, replace isAuthenticated with useAuth
    
    * SCRUM-122 feat: adjust paddings
    
    * SCRUM-122 chore: remove open post button from main page

---
+++ src/components/PostDetailsModal/PostDetailsModal.tsx
@@ -0,0 +1,117 @@
+'use client'
+import {
+  ActionMenu,
+  Avatar,
+  AvatarBlock,
+  Button,
+  EditOutline,
+  Modal,
+  Scroll,
+  TrashOutline,
+  Typography,
+} from '@candy.thieves/ui-kit-lumos'
+import { PostActions } from '@/components/Post/PostActions/PostActions'
+import { PostComments } from '@/components/Post/PostComments/PostComments'
+import { PostImagesCarousel } from '@/components/PostImagesCarousel/PostImagesCarousel'
+import { useIsMobileViewport } from '@/lib/hooks'
+import { useAuth } from '@/lib/hooks/useAuth'
+import { getPostImageAreaStyle } from '@/lib/utils'
+import { mockComments, mockLikedByUsers, type Post } from './PostDetailsModal.mock'
+import s from './PostDetailsModal.module.scss'
+
+type Props = {
+  post: Post
+  open: boolean
+  onClose: () => void
+  onEdit: () => void
+}
+
+export const PostDetailsModal = ({ post, open, onClose, onEdit }: Props) => {
+  const { user, isAuthenticated } = useAuth()
+  const isMobileViewport = useIsMobileViewport()
+  const isAuthor = !!user && user.id === post.userId
+
+  return (
+    <Modal
+      open={open}
+      onClose={onClose}
+      size={'xl'}
+      closeButtonOutside
+      fullSize
+      className={s.modal}
+    >
+      <div className={s.postContainer} style={getPostImageAreaStyle(post.images[0])}>
+        <div className={s.postImageContainer}>
+          <PostImagesCarousel images={post.images} alt={post.description || 'Post'} />
+        </div>
+
+        <div className={s.postInfo}>
+          {/* Header */}
+          <div className={s.postHeader}>
+            <div className={s.author}>
+              <Avatar userName={post.userName} size={'s'} delayMs={0} />
+
+              <Typography variant={'subtitle2'}>{post.userName}</Typography>
+            </div>
+
+            {isAuthor && (
+              <ActionMenu
+                ariaLabel={'Open post actions'}
+                items={[
+                  {
+                    icon: <EditOutline size={24} />,
+                    id: 'edit-post',
+                    label: 'Edit Post',
+                    onSelect: onEdit,
+                  },
+                  {
+                    icon: <TrashOutline size={24} />,
+                    id: 'delete-post',
+                    label: 'Delete Post',
+                    onSelect: () => {},
+                  },
+                ]}
+              />
+            )}
+          </div>
+
+          {/* Scrollable: author's description + comments */}
+          <div className={`${s.scrollSection} ${!isAuthenticated ? s.scrollSectionGuest : ''}`}>
+            <Scroll orientation={'vertical'}>
+              <PostComments post={post} comments={mockComments} />
+            </Scroll>
+          </div>
+
+          {/* Actions */}
+          {isAuthenticated && <PostActions showComments={isMobileViewport} />}
+
+          {/* Footer: likes + date */}
+          <div className={`${s.postFooter} ${!isAuthenticated ? s.postFooterGuest : ''}`}>
+            <div className={s.likes}>
+              <AvatarBlock users={mockLikedByUsers} />
+
+              <Typography variant={'body2'}>2 243 &quot;Like&quot;</Typography>
+            </div>
+
+            <div className={s.date}>
+              <Typography variant={'caption1'} color={'var(--color-light-900)'}>
+                July 3, 2021
+              </Typography>
+            </div>
+          </div>
+
+          {/* Comment input */}
+          {isAuthenticated && (
+            <div className={s.commentForm}>
+              <input className={s.commentInput} placeholder={'Add a Comment...'} />
+
+              <Button type={'button'} variant={'text'}>
+                Publish
+              </Button>
+            </div>
+          )}
+        </div>
+      </div>
+    </Modal>
+  )
+}

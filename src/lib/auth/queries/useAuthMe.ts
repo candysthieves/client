5f0dabf4d20ee77383f6923ce9404ee60b85d21b
Vlad Skalski <vlad.skalsky.96@gmail.com>
2026-09-06 00:32:49 +0200

    SCRUM-145 fix: re-apply useAuthMe fix, reverting it broke post modal for guests

--- src/lib/auth/queries/useAuthMe.ts
+++ src/lib/auth/queries/useAuthMe.ts
@@ -4,25 +4,13 @@ import { authKeys } from '@/lib/auth'
 import { ACCESS_TOKEN_LS_KEY } from '@/lib/model'
 
 export function useAuthMe() {
-  // Commented due to Hydration Mismatch
-  // const token = typeof window !== 'undefined' ? localStorage.getItem(ACCESS_TOKEN_LS_KEY) : null
+  const token = typeof window !== 'undefined' ? localStorage.getItem(ACCESS_TOKEN_LS_KEY) : null
 
   return useQuery({
     queryKey: authKeys.me(),
-    // queryFn: authMe,
-    queryFn: () => {
-      console.log('🔥 ACTUAL authMe HTTP REQUEST')
-
-      return authMe()
-    },
-
+    queryFn: authMe,
     retry: false,
-    // enabled: !!token, // check if refresh token will not work properly
-    // staleTime: 5 * 60 * 1000,
-    // staleTime: Infinity,
-    // refetchOnMount: false,
-    // refetchOnWindowFocus: false,
-    // refetchOnReconnect: false,
+    enabled: !!token, // check if refresh token will not work properly
   })
 }
 

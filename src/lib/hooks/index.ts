f9049b14eda2a88651642101bb518de5c15a68bc
Andrei <107413715+Intrstng@users.noreply.github.com>
2026-09-08 02:51:19 +0300

    SCRUM-136 feat: add links highlihtning in Sidebar and Menu components (#42)

--- src/lib/hooks/index.ts
+++ src/lib/hooks/index.ts
@@ -1,3 +1,4 @@
 export { useIsMobileViewport } from './isMobileViewport'
 export { useAuth } from './useAuth'
 export { usePostEvents } from './usePostEvents'
+export { useActiveMenuItem } from './useActiveMenuItem'

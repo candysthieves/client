41c103711bf2ec814c4e6330bcd32056374ca5bc
Intrstng <intrstg@gmail.com>
2026-09-20 20:57:12 +0300

    SCRUM-145 feat: add blank pages for Favorites, Search and Statistics

---
+++ src/app/(protected)/statistics/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { Typography } from '@candy.thieves/ui-kit-lumos'
+
+export default function Statistics() {
+  return (
+    <main>
+      <h1>Statistics</h1>
+      <Typography variant={'caption1'}>Statistics content</Typography>
+    </main>
+  )
+}

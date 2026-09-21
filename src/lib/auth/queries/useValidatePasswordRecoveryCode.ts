ffdcce198e4c626303b15d39116f9ad3e43883e1
Andrei <107413715+Intrstng@users.noreply.github.com>
2026-09-21 23:55:21 +0300

    SCRUM-108 fix: add return value from useValidatePasswordRecoveryCode query (#55)

--- src/lib/auth/queries/useValidatePasswordRecoveryCode.ts
+++ src/lib/auth/queries/useValidatePasswordRecoveryCode.ts
@@ -5,10 +5,10 @@ import { authKeys } from '@/lib/auth'
 export function useValidatePasswordRecoveryCode(recoveryCode: string) {
   return useQuery({
     queryKey: authKeys.validatePasswordRecoveryCode(recoveryCode),
-    queryFn: () =>
-      validatePasswordRecoveryCode({
-        recoveryCode,
-      }),
+    queryFn: async () => {
+      await validatePasswordRecoveryCode({ recoveryCode })
+      return true // empty body in response with 204 - useQuery can not return undefined
+    },
     enabled: Boolean(recoveryCode),
   })
 }
